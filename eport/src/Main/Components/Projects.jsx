adad41774f4453861494e6f39fc9940c3515dae8
trithecoder <triyelnewry@gmail.com>
2023-11-18 19:45:23 -0500

    Mobile Optimizations

--- eport/src/Main/Components/Projects.jsx
+++ eport/src/Main/Components/Projects.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-import "../Components/Eport.scss"
+import "../Components/Eport.scss";
 import Huddle from "./Projects/Huddle";
 import Netflix from "./Projects/Netflix";
 
 const Projects = () => {
   return (
     <div className="projects">
-      <section id="projects" className="projects">
-        <div>
-          <header>
-            <h1>Projects</h1>
-          </header>
-          <nav>
-            <div className="project-class">
-              <figure>
-                <Netflix />
-              </figure>
-            </div>
-            <div className="project-class">
-              <figure>
-                <Huddle />
-              </figure>
-            </div>
-          </nav>
+      <section id="projects" className="project-sect">
+        <div className="content-wrapper">
+          <div>
+            <header>
+              <h1>Projects</h1>
+            </header>
+            <nav>
+              <div className="project-class">
+                <figure>
+                  <Netflix />
+                </figure>
+              </div>
+              <div className="project-class">
+                <figure>
+                  <Huddle />
+                </figure>
+              </div>
+            </nav>
+          </div>
         </div>
       </section>
     </div>
