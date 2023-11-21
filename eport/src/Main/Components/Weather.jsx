7374cd2309fed1d7d6873ee36a04c51904fedca7
trithecoder <triyelnewry@gmail.com>
2023-11-21 10:00:38 -0500

    Mobile Weather App//Animation Update

--- eport/src/Main/Components/Weather.jsx
+++ eport/src/Main/Components/Weather.jsx
@@ -1,40 +1,12 @@
-import React from "react";
-import WeatherApp from "./Weatherapp/Weather";
-import MobileWeatherApp from "./Weatherapp/Weather";
-import {
-  motion,
-  AnimatePresence,
-  animate,
-  useScroll,
-  transform,
-  useTransform,
-} from "framer-motion";
-import { BrowserView, MobileOnlyView } from "react-device-detect";
+import React from 'react'
+import WeatherApp from './Weatherapp/Weather'
+import { motion, AnimatePresence, animate, useScroll, transform, useTransform } from "framer-motion";
 
 const Weather = () => {
   return (
-    <>
-      <BrowserView>
-        <motion.div
-          id="checkweather"
-          initial={{ opacity: 0, y: 75, }}
-          whileInView={{
-            opacity: 1,
-            transition: { duration: 1 },
-            transition: {
-              duration: 1,
-            },
-            y: 0,
-          }}
-        >
-          <WeatherApp />
-        </motion.div>
-      </BrowserView>
-      <MobileOnlyView>
-        <MobileWeatherApp />
-      </MobileOnlyView>
-    </>
-  );
-};
+    <motion.div id='checkweather' initial={{opacity: 0}} whileInView={{opacity: 1, transition: {duration: 1} }}><WeatherApp/></motion.div>
+    
+  )
+}
 
-export default Weather;
+export default Weather
