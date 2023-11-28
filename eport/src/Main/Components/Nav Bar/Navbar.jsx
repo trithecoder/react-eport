2bd45156a3f8c955ed4e48fa639bc50871d8107c
trithecoder <triyelnewry@gmail.com>
2023-11-27 20:06:15 -0500

    Mobile NavBar Update

--- eport/src/Main/Components/Nav Bar/Navbar.jsx
+++ eport/src/Main/Components/Nav Bar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import NavbarItems from "./NavbarItems";
 import { motion, AnimatePresence } from "framer-motion";
-import "./Navbar.scss";
+import "./NavBar.scss";
 
 const Navbar = () => {
   const [isToggled, setToggle] = useState(false);
